ed961ccd305a3cd4abf276dcd3d4892f6236c51b
jj4252 <jj4252@github.com>
2024-11-15 11:35:38 -0500

    commit

--- code/preprocess.py
+++ code/preprocess.py
@@ -7,21 +7,36 @@ def merge_parquets(dir):
     """
     Read and Merge all the parquet files 
     """
+    # setting appropriate file path 
     data_dir = Path(dir)
     
+    # concatenate all the parquet files and merge them 
     full_data = pd.concat(
         pd.read_parquet(parquet_file) 
         for parquet_file in data_dir.glob('*.parquet')
     )
-
+    full_data = full_data[full_data.columns[:-1]]
+    # Make sure there are no duplicate rows in the dataset 
+    full_data.drop_duplicates(inplace = True)
     return full_data
 
 
 def sample_dat(dat, prop):
     """
-    
+    docstring 
     """
+    # uniform sample of the indices 
     sample_idx = np.random.randint(0, len(dat), int(len(dat) * prop))
     return dat.iloc[sample_idx]
+
+def drop_missing_vals(dat, column):
+    """
+    docstring
+    """
+    dat_na_dropped = dat[dat[column].notnull()] 
+    return dat_na_dropped 
+
+
+
 
 
