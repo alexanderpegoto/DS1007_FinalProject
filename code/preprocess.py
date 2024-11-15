51294b10123627751ad8e11855a8c5675c3f7418
jiwoo Jeong <jiwoojeong@jiwoos-mbp.mynetworksettings.com>
2024-11-15 01:25:56 -0500

    Few changes

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,1 +1,20 @@
+import numpy as np
+import pandas as pd 
+from pathlib import Path 
+import os 
+
+def merge_parquets(dir):
+    """
+    Read and Merge all the parquet files 
+    """
+    data_dir = Path(dir)
+    
+    full_data = pd.concat(
+        pd.read_parquet(parquet_file) 
+        for parquet_file in data_dir.glob('*.parquet')
+    )
+
+    return full_data
+
+
 
