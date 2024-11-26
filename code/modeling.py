ead56baea900cdb68978670e6f4538fc930b9307
jj4252 <jj4252@github.com>
2024-11-26 16:26:31 -0500

    update on modeling

--- code/modeling.py
+++ code/modeling.py
@@ -1,3 +1,6 @@
+import pandas as pd 
+from statsmodels.tsa.seasonal import STL
+import matplotlib.pyplot as plt
 
 ## Time Series Modeling - Trend, Seasonality decomposition 
 def decompose(dat, column):
@@ -6,6 +9,7 @@ def decompose(dat, column):
     """
     dat[column] = pd.to_datetime(dat[column])
     dat.set_index(column, inplace = True)
+    stl = STL(dat, seasonal = 7)
     result = stl.fit()
     plt.figure(figsize = (10,13))
     result.plot()
